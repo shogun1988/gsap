39a92152ac324a9784f3c8503560e830984553f5
shogun1988 <nokahvandenhoven@gmail.com>
2025-11-19 12:37:41 +0100

    implement art section

--- constants/index.js
+++ constants/index.js
@@ -86,19 +86,19 @@ export const mockTailLists = [
 //  },
 // ];
 
-// export const featureLists = [
-//  "Perfectly balanced blends",
-//  "Garnished to perfection",
-//  "Ice-cold every time",
-//  "Expertly shaken & stirred",
-// ];
+export const featureLists = [
+ "Perfectly balanced blends",
+ "Garnished to perfection",
+ "Ice-cold every time",
+ "Expertly shaken & stirred",
+];
 
-// export const goodLists = [
-//  "Handpicked ingredients",
-//  "Signature techniques",
-//  "Bartending artistry in action",
-//  "Freshly muddled flavors",
-// ];
+export const goodLists = [
+ "Handpicked ingredients",
+ "Signature techniques",
+ "Bartending artistry in action",
+ "Freshly muddled flavors",
+];
 
 // export const storeInfo = {
 //  heading: "Where to Find Us",
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About"
+import Art from "./components/Art";
+import Menu from "./components/Menu";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -13,7 +15,9 @@ const App = () => {
             <Navbar />
             <Hero />
             <Cocktails /> 
-            <About />       
+            <About />
+            <Art />
+            <Menu />     
         </main>
     )
 }
